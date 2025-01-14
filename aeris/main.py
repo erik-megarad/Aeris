b11051bb2b5c42f8ad613570aee97e46d6ca2051
Erik Peterson <e@eriklp.com>
2025-01-13 17:00:14 -0700

    Initial commit for Aeris

---
+++ aeris/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from ariadne import QueryType, make_executable_schema
+from ariadne.asgi import GraphQL
+
+# Define GraphQL schema
+type_defs = """
+type Query {
+    hello: String!
+}
+"""
+
+query = QueryType()
+
+@query.field("hello")
+def resolve_hello(_, info):
+    return "Welcome to Aeris!"
+
+schema = make_executable_schema(type_defs, query)
+
+app = FastAPI()
+
+app.add_route("/graphql", GraphQL(schema))
+
